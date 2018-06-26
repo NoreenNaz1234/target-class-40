4dc85e31c3c7ba91d7f48e9af2c7876839e38d01
VB Student <vbstudent@uit.edu>
2018-06-26 17:52:12 +0500

    target

---
+++ script.js
@@ -0,0 +1,31 @@
+function cal() {
+     
+    let body = document.querySelector("body");
+
+	let para = document.querySelector("p");
+	for(var i = 1; i<=100; i++){
+		body.innerHTML = body.innerHTML + "<p>" + i + "</p>";
+    
+}
+	
+
+}
+
+
+// function cal() {
+// 	var min = 0;
+// 	var max = 100;
+// 	let body = document.querySelector("body");
+// 	let input1 = document.querySelector("#in1").value;
+// 	let input2 = document.querySelector("#in2").value;
+
+   
+//     for(var i = input1 ; i <=input2; i++){
+//     	body.innerHTML = body.innerHTML + "<p>" + i + "</p>";
+
+
+//     }
+  
+
+
+}
